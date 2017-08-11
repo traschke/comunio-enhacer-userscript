6bc60bccd0a870b3862e2b536e79804340a27e2d
Timo Raschke <timo.raschke@gmail.com>
2017-08-11 15:46:41 +0200

    - Create Map of playerIds and tradables - Use Promises - Add Link to comanalytics in exchangemarktet

--- ComunioEnhancer.user.js
+++ ComunioEnhancer.user.js
@@ -23,72 +23,115 @@
         console.log('Waiting for market being loaded...');
         checkIsAvailable('exchangemarket_purchase')
         .then(() => {
-        console.log('Market loaded!');
-            exchangeMarketTools.calcGewinn();
-            exchangeMarketTools.markUsers();
-            exchangeMarketTools.updateHrefsToComstats();
+            console.log('Market loaded!');
+            exchangeMarketTools.updateExchangeMarket();
         });
     };
 
     let exchangeMarketTools = {
-        calcGewinn: () => {
-            var tradables = $('.market .tradable');
-            $.each(tradables, function (index, tradable) {
-                let quoteElems = $(tradable).find('.quote');
-                let recommendElems = $(tradable).find('.recommend');
-
-                let quote = parseInt(quoteElems[0].innerHTML.replace(/\./g,''));
-                let recommend = parseInt(recommendElems[0].innerHTML.replace(/\./g,''));
-
-                let gewinn = quote - recommend;
-
-                let marketvalue = $(tradable).find('.marketvalue')[0];
-                var label = $(tradable).find('.label')[0];
-
-                let color = '#000';
-
-                if (gewinn > 0) {
-                    color = '#8CBF3F';
-                } else if (gewinn < 0) {
-                    color = '#E9322E';
-                }
-
-                if (gewinn !== 0) {
-                    gewinn = numberWithCommas(gewinn)
-                } else {
-                    gewinn = '-';
-                }
-                $('<div class="recommend" style="color: ' + color + '">' + gewinn + '</div>').appendTo(marketvalue);
-                $('<div>Gewinn</div>').appendTo(label);
-            });
+        updateExchangeMarket: () => {
+            exchangeMarketTools.getTradablesMap()
+                .then(exchangeMarketTools.calcGewinn)
+                .then(exchangeMarketTools.markUsers)
+                .then(exchangeMarketTools.updateHrefsToComstats)
+                .then(exchangeMarketTools.addComAnalyticsLink);
         },
-        markUsers: () => {
-            let tradables = $('.market .tradable');
-            $.each(tradables, function (index, tradable) {
-                let username = $(tradable).find('.username-name')[0].innerText;
-
-                if (username.includes('(Du)')) {
-                    $(tradable).css('background', '#94c4d1');
-                } else if (username !== 'Computer') {
-                    $(tradable).css('background', '#dbbfbe');
-                }
-            });
-        },
-        updateHrefsToComstats: () => {
-            let tradables = $('.market .tradable');
-            $.each(tradables, function (index, tradable) {
-                let playerNameEle = $(tradable).find('.name')[0];
-                let originalHref = $(playerNameEle).attr("href");
-                let playerId = originalHref.split('-')[1];
-                $(playerNameEle).attr('href', 'http://stats.comunio.de/profil.php?id=' + playerId);
-                $(playerNameEle).click((event) => {
-                    event.preventDefault();
-                    let newWindow = window.open($(playerNameEle).attr('href'),'','height=630,width=600');
-                    if (window.focus) {
-                        newWindow.focus();
-                    }
-                    return false;
+        getTradablesMap: () => {
+            return new Promise((resolve, reject) => {
+                let tradablesMap = new Map();
+                let tradables = $('.market .tradable');
+                $.each(tradables, function (index, tradable) {
+                    let playerNameEle = $(tradable).find('.name')[0];
+                    let originalHref = $(playerNameEle).attr("href");
+                    let playerId = originalHref.split('-')[1];
+                    tradablesMap.set(playerId, tradable);
                 });
+                return resolve(tradablesMap);
+            });
+        },
+        addComAnalyticsLink: (tradablesMap) => {
+            return new Promise((resolve, reject) => {
+                tradablesMap.forEach((tradable, id, map) => {
+                    let playerNameEle = $(tradable).find('.name')[0];
+                    let playerNameEleParent = $(playerNameEle).parent();
+                    let divii = $('<div></div>');
+                    let comAnalyticsEntry = $('<a href="http://com-analytics.de/player/' + id + '"><img src="http://com-analytics.de/favicon.ico"></a>');
+                    $(divii).append($(comAnalyticsEntry));
+                    $(playerNameEleParent).append($(divii));
+                    $(comAnalyticsEntry).click((event) => {
+                        event.preventDefault();
+                        let newWindow = window.open($(comAnalyticsEntry).attr('href'),'','height=630,width=720');
+                        if (window.focus) {
+                            newWindow.focus();
+                        }
+                        return false;
+                    });
+                });
+                return resolve(tradablesMap);
+            });
+        },
+        calcGewinn: (tradablesMap) => {
+            return new Promise((resolve, reject) => {
+                tradablesMap.forEach((tradable, id, map) => {
+                    let quoteElems = $(tradable).find('.quote');
+                    let recommendElems = $(tradable).find('.recommend');
+
+                    let quote = parseInt(quoteElems[0].innerHTML.replace(/\./g,''));
+                    let recommend = parseInt(recommendElems[0].innerHTML.replace(/\./g,''));
+
+                    let gewinn = quote - recommend;
+
+                    let marketvalue = $(tradable).find('.marketvalue')[0];
+                    var label = $(tradable).find('.label')[0];
+
+                    let color = '#000';
+
+                    if (gewinn > 0) {
+                        color = '#8CBF3F';
+                    } else if (gewinn < 0) {
+                        color = '#E9322E';
+                    }
+
+                    if (gewinn !== 0) {
+                        gewinn = numberWithCommas(gewinn)
+                    } else {
+                        gewinn = '-';
+                    }
+                    $('<div class="recommend" style="color: ' + color + '">' + gewinn + '</div>').appendTo(marketvalue);
+                    $('<div>Gewinn</div>').appendTo(label);
+                });
+                return resolve(tradablesMap);
+            });
+        },
+        markUsers: (tradablesMap) => {
+            return new Promise((resolve, reject) => {
+                tradablesMap.forEach((tradable, id, map) => {
+                    let username = $(tradable).find('.username-name')[0].innerText;
+
+                    if (username.includes('(Du)')) {
+                        $(tradable).css('background', '#94c4d1');
+                    } else if (username !== 'Computer') {
+                        $(tradable).css('background', '#dbbfbe');
+                    }
+                });
+                return resolve(tradablesMap);
+            });
+        },
+        updateHrefsToComstats: (tradablesMap) => {
+            return new Promise((resolve, reject) => {
+                tradablesMap.forEach((tradable, id, map) => {
+                    let playerNameEle = $(tradable).find('.name')[0];
+                    $(playerNameEle).attr('href', 'http://stats.comunio.de/profil.php?id=' + id);
+                    $(playerNameEle).click((event) => {
+                        event.preventDefault();
+                        let newWindow = window.open($(playerNameEle).attr('href'),'','height=630,width=600');
+                        if (window.focus) {
+                            newWindow.focus();
+                        }
+                        return false;
+                    });
+                });
+                return resolve(tradablesMap);
             });
         }
     };
@@ -125,7 +168,6 @@
         .then(() => {
         setInterval(() => {
             if (pathname !== window.location.pathname) {
-                //console.log('URL changed from', pathname, 'to', window.location.pathname);
                 pathname = window.location.pathname;
                 switch (pathname) {
                     case '/exchangemarket':
